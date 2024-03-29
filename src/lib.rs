d3cfdc607281904e2971f038ff951eb6e2a22480
dcattaneo <dcattaneo@gmail.com>
2024-03-29 22:43:39 +0100

    first test

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(address: &str) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind(address)?
+    .run();
+    //removed .await
+
+    Ok(server)
+}
