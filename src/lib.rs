fa46ed1a7b32bcfb7c651c79a15f9d095f77f3dd
dcattaneo <dcattaneo@gmail.com>
2024-11-12 17:27:18 +0100

    added some tests and db scripts

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, /*HttpRequest,*/ HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, /*HttpRequest,*/ HttpResponse, HttpServer /* , Responder*/};
 use std::net::TcpListener;
 
 /* async fn greet(req: HttpRequest) -> impl Responder {
@@ -7,8 +7,26 @@ use std::net::TcpListener;
     format!("Hello {}!", &name)
 } */
 
-async fn health_check() -> impl Responder {
+// We were returning `impl Responder` at the very beginning.
+/*async fn health_check() -> impl Responder {
     HttpResponse::Ok()
+}*/
+
+// We are now spelling out the type explicitly given that we have
+// become more familiar with `actix-web`.
+// There is no performance difference! Just a stylistic choice :)
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
@@ -17,6 +35,8 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             //.route("/", web::get().to(greet))
             //.route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
