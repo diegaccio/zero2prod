ebf8dbb7955387edeed6b22bce1d11071567287d
dcattaneo <dcattaneo@gmail.com>
2024-11-12 11:46:59 +0100

    removed warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, /*HttpRequest,*/ HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
+/* async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
-}
+} */
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
