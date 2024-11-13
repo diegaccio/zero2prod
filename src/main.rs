2490d3d0271dc556e3e280d9660b500cc9be004d
dcattaneo <dcattaneo@gmail.com>
2024-11-13 18:20:05 +0100

    settings file management added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
     //Bubbleup the io::Errorifwe failed tobindtheaddress
     //Otherwise call.await on our Server
-
-    run(TcpListener::bind("127.0.0.1:8000").expect("BIND FAILED"))?.await
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
