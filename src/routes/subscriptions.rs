0508dbe8e90872ecc39d2cdbbb051f5b6c8b3105
dcattaneo <dcattaneo@gmail.com>
2024-11-20 00:20:29 +0100

    tracing update

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -15,68 +14,41 @@ pub struct FormData {
 //web::Data, when a new request comes in, computes the TypeId of the type you specified in the signature (in our case PgConnection) and checks if there is a record corresponding to it in the type- map.
 //If there is one, it casts the retrieved Any value to the type you specified (TypeId is unique, nothing to worry about) and passes it to your handler.
 //It is an interesting technique to perform what in other language ecosystems might be referred to as dependency injection.
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    // Retrieving a connection from the application state!
-    pool: web::Data<PgPool>,
-) -> HttpResponse {
-    // Let's generate a random unique identifier
-    let request_id = Uuid::new_v4();
+#[tracing::instrument(
+    name = "Adding a new subscriber", skip(form, pool),
+    fields(
+    request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name= %form.name
+    ) )]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    // Spans, like logs, have an associated level // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name= %form.name
-    );
-    //Notice that we prefixed all of them with a % symbol: we are telling tracing to use their Display implementation for logging purposes
-
-    // Using `enter` in an async function is a recipe for disaster!
-    // Bear with me for now, but don't do this at home.
-    // See the following section on `Instrumenting Futures`
-    let _request_span_guard = request_span.enter();
-
-    // `_request_span_guard` is dropped at the end of `subscribe`
-    // That's when we "exit" the span
-
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the query future lifetime
-    //this trace will show up every time the future is polled
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    let res = sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
-        "#,
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+    "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    // First we attach the instrumentation, then we `.await` it
-    .instrument(query_span)
-    .await;
-
-    match res {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+        // Using the `?` operator to return early
+        // if the function failed, returning a sqlx::Error
+        // We will talk about error handling in depth later!
+    })?;
+    Ok(())
 }
