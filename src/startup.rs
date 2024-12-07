8008f43ecedac2c33f9567534329f555687b2511
dcattaneo <dcattaneo@gmail.com>
2024-12-07 19:36:19 +0100

    chapter 4 done

--- src/startup.rs
+++ src/startup.rs
@@ -4,6 +4,7 @@ use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
@@ -14,7 +15,9 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares are added using the `wrap` method on `App`
-            .wrap(Logger::default())
+            //.wrap(Logger::default())
+            // Instead of `Logger::default`
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
