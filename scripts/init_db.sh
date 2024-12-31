28dc42537150ecbe9760102be48a03279556f65d
dcattaneo <dcattaneo@gmail.com>
2024-12-31 14:24:43 +0100

    security check

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -10,7 +10,7 @@ fi
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed. Check this link: https://www.timescale.com/blog/how-to-install-psql-on-mac-ubuntu-debian-windows/"
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
   echo >&2 "to install it."
   exit 1
 fi
